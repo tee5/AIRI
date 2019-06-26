40806052e41269e697a0f08ebe73b95488b1bcad
tee5 <noppe18@gmail.com>
2019-06-26 11:28:04 +0900

    update DDL

--- api/models.py
+++ api/models.py
@@ -13,6 +13,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "user"
     empno = Column(String(4), primary_key=True, nullable=False)
+    password = Column(String(255), primary_key=True, nullable=False)
     firstname_ja = Column(String(255), nullable=False)
     lastname_ja = Column(String(255), nullable=False)
     firstname_en = Column(String(255), nullable=False)
@@ -33,11 +34,22 @@ class User(Base):
             "email": self.email,
             "created_at": self.created_at.isoformat(),
             "updated_at": self.updated_at.isoformat(),
-            "deleted": self.deleted
+            "deleted": self.deleted,
         }
 
     def __repr__(self):
-        return "<User(empno={empno}>".format(empno=self.empno)
+        return "<User(empno={empno}, password={password}, firstname_ja={firstname_ja}, lastname_ja={lastname_ja}, firstname_en={firstname_en}, lastname_en={lastname_en}, email={email}, created_at={created_at}, updated_at={updated_at}, deleted={deleted}>".format(
+            empno=self.empno,
+            password="****",
+            firstname_ja=self.firstname_ja,
+            lastname_ja=self.lastname_ja,
+            firstname_en=self.firstname_en,
+            lastname_en=self.lastname_en,
+            email=self.email,
+            created_at=self.created_at.isoformat(),
+            updated_at=self.updated_at.isoformat(),
+            deleted=self.deleted,
+        )
 
 
 db_connect = os.environ["db_connect"]
